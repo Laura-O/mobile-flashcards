3d6fa580c47d612d2afd72eb15eac08d621118a4
Laura-O <laura.ohrndorf@gmail.com>
2017-10-15 18:52:28 +0200

    feat: Single decks on list

--- components/Deck.js
+++ components/Deck.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
+import {purple, white} from '../utils/colors'
 
-class Deck extends Component {
-	render() {
-		return <Text>Deck</Text>;
-	}
+export default function Deck ({deck}) {
+	return(
+		<View>		
+			<Text style={styles.deck}>{deck.title}</Text>
+		</View>
+	)	
 }
 
-export default connect()(Deck);
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	deck: {
+    backgroundColor: purple,
+		color: white,
+    borderRadius: 2,
+    justifyContent: 'center',
+		fontSize: 32,
+    textAlign: 'center',
+		marginTop: 20,
+  },
+})
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {connect} from 'react-redux';
+import Deck from './Deck'
 
-class DeckList extends Component {
+class DeckList extends Component {	
+	
 	render() {
-		const decks = Object.keys(this.props.decks);		
+		const decks = Object.keys(this.props.decks);				
+		{console.log(this.props.decks['JavaScript'])}
 
 		if (!decks) {
 			<Text>No decks found</Text>;
 		}
-		return (
-			<View>
+		return (			
+			<View>				
 				{decks.map(deck => (
-					<Text key={deck} deck={this.props.decks[deck]}>{deck}</Text>					
+					<Deck key={deck} deck={this.props.decks[deck]} />
 				))}
 			</View>
 		);
